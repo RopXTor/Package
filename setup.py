0fbf62fdb12b5bd4e6cdde7c232ddd3dfbd6aac9
RopXTor <julien.lorioux18@gmail.com>
2025-02-01 22:20:38 +0100

    -- files

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
+# pip install setuptools
+
 from setuptools import setup, find_packages
 
-requirements = ["toml", "PyYAML"]
+requirements = ["toml", "PyYAML", "pandas", "sqlalchemy", "numpy"]
 
 setup(
     name="helpers",
-    version="1.0",
+    version="1.4",
     install_requires=requirements,
     packages=find_packages(),
-    author="your_name",
+    author="fab",
 )
-
-
